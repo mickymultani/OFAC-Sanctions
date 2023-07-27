b6d20593fd57f7c732bc771638bbd3595f68ba88
mickymult <42827572+mickymult@users.noreply.github.com>
2023-07-26 20:23:15 -0600

    Update fetch_and_process.py

--- fetch_and_process.py
+++ fetch_and_process.py
@@ -1,5 +1,6 @@
 import requests
 import subprocess
+import os
 
 def download_xml_and_run_script():
     url = 'https://www.treasury.gov/ofac/downloads/sanctions/1.0/sdn_advanced.xml'
@@ -7,7 +8,10 @@ def download_xml_and_run_script():
     with open('sdn_advanced.xml', 'w') as file:
         file.write(response.text)
 
+    # Get the current working directory
+    cwd = os.getcwd()
+
     # Call your script here
-    subprocess.call(["python", "process_xml.py", "sdn_advanced.xml"])
+    subprocess.call(["python", "process_xml.py", "--output-path", cwd, "sdn_advanced.xml"])
 
 download_xml_and_run_script()
